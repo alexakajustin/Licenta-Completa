af7aee0f55d19bf0bdbf9d06ffc745ff0b1fc2d2
alexakajustin <alexakajustin@gmail.com>
2026-05-03 21:38:31 +0300

    reordered

--- procedural-terrain/resources/shaders/debug.frag
+++
@@ -1,7 +0,0 @@
-#version 420
-
-layout(location = 0) out vec4 fragmentColor;
-
-in vec3 fColor;
-
-void main() { fragmentColor = vec4(fColor, 1.0); }
--- procedural-terrain/src/hdr.h
+++
@@ -1,9 +0,0 @@
-#include <glad/glad.h>
-
-#include <string>
-#include <vector>
-
-namespace gpu {
-  GLuint loadHdrTexture(const std::string &filename);
-  GLuint loadHdrMipmapTexture(const std::vector<std::string> &filenames);
-}  // namespace gpu
